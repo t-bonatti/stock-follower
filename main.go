5239a4900a61cbee8a78e0fb4546dc7606d11665
t-bonatti <tiago.bonatti@gmail.com>
2019-10-21 22:33:48 -0300

    fix: remove comments

--- main.go
+++ main.go
@@ -5,12 +5,5 @@ import (
 )
 
 func main() {
-	// resp, err := http.Get("http://www37.bb.com.br/portalbb/redeAtendimento/GetAgenciasPorPrefixo.bbx?str=" + strconv.Itoa(code))
-	// if err != nil {
-	// 	return
-	// }
-	// defer func() {
-	// 	err := resp.Body.Close()
-	// }()
 	fmt.Println("Hello world")
 }
